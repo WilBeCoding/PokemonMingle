8cd499b5d0e4ff1df5135c08734d120629b95b42
Wilson Rondini <wilbecoding@gmail.com>
2016-07-12 01:24:39 -0600

    Route and form setup for signup page. NEEDS STYLING.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'PokéMingle' });
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup', {title: 'Sign Up'})
+})
+
 module.exports = router;
