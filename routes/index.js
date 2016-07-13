fd1a4a6eb1a856ce1bf02f46a346756601689af6
Wilson Rondini <wilbecoding@gmail.com>
2016-07-12 20:13:00 -0600

    Removed bower. Added some side pages

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/pokemingle');
+var usersCollection = db.get('users');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +11,15 @@ router.get('/', function(req, res, next) {
 
 router.get('/signup', function(req, res, next) {
   res.render('signup', {title: 'Sign Up'})
+})
+
+router.get('/profile', function(req,res,next){
+  res.render('profile', {title: 'Profile'})
+})
+
+router.post('/signup', function(req, res, next){
+  usersCollection.insert({username: req.body.username, age: req.body.userage, sex: req.body.usersex, country: req.body.usercountry, zip: req.body.userzip, email: req.body.email, password: req.body.password})
+  res.redirect('/');
 })
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost:27017/pokemingle');
+var usersCollection = db.get('users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', {title: 'Sign Up'})
+})
+
+router.get('/profile', function(req,res,next){
+  res.renger('profile', {title: 'Profile'})
+})
+
+router.post('/signup', function(req, res, next){
+  usersCollection.insert({username: req.body.username, age: req.body.userage, sex: req.body.usersex, country: req.body.usercountry, zip: req.body.userzip, email: req.body.email, password: req.body.password})
+  res.redirect('/');
+})
 
 module.exports = router;
